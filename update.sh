3906f80e4638f46fe1abf6b6933fc2a4551fac77
Yannick Weiss <hi@yannick.dev>
2020-06-01 11:52:58 +0200

    Adding yq dependency to repo
    I trade a bigger Git repo for less dependency toil any day.

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ set -e
 
 [ -z "$BUILD_VERSION" ] && echo "BUILD_VERSION is required." && exit 1;
 
-yq w -i "01-prod/deployment-prod.yaml" "spec.template.spec.containers[0].image" "nginx:$BUILD_VERSION"
+./yq w -i "01-prod/deployment-prod.yaml" "spec.template.spec.containers[0].image" "nginx:$BUILD_VERSION"
 
 git checkout -b "release-$BUILD_VERSION" master
 git add "01-prod/deployment-prod.yaml" && git commit -m "Updated to build $BUILD_VERSION"
